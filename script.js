701fda84fe40961d8aee3dd35313153d7e4b30bd
Jitka Badhan Nebeska <jitkabadhan@gmail.com>
2021-03-30 22:11:38 +0200

    Points calculator, (somehow) working in basic skelet of the game

--- script.js
+++ script.js
@@ -1,93 +1,88 @@
 'use strict';
 
 // Define the variables
-let pictures = [];
+const pictures = [
+  document.getElementById('pic1'),
+  document.getElementById('pic2'),
+  document.getElementById('pic3'),
+  document.getElementById('pic4'),
+  document.getElementById('pic5'),
+  document.getElementById('pic6'),
+  document.getElementById('pic7'),
+  document.getElementById('pic8'),
+  document.getElementById('pic9'),
+  document.getElementById('pic10'),
+  document.getElementById('pic11'),
+  document.getElementById('pic12'),
+];
+const pointsDiv = document.getElementById('points');
 let compare = [];
 let points = 0;
 
-// 'Connect' these variables with the objects on the website
-const pic1 = document.getElementById('pic1');
-const pic2 = document.getElementById('pic2');
-const pic3 = document.getElementById('pic3');
-const pic4 = document.getElementById('pic4');
-const pic5 = document.getElementById('pic5');
-const pic6 = document.getElementById('pic6');
-const pic7 = document.getElementById('pic7');
-const pic8 = document.getElementById('pic8');
-const pic9 = document.getElementById('pic9');
-const pic10 = document.getElementById('pic10');
-const pic11 = document.getElementById('pic11');
-const pic12 = document.getElementById('pic12');
+// Propojené počítadlo:
+const showPoints = () => {
+  pointsDiv.textContent = `Body: ${points}`;
+};
+showPoints();
 
-// Insert the variables of pictures in the predefined array
-pictures = [
-  pic1,
-  pic2,
-  pic3,
-  pic4,
-  pic5,
-  pic6,
-  pic7,
-  pic8,
-  pic9,
-  pic10,
-  pic11,
-  pic12,
-];
+// Define the functions used later in global scope
+function display(item) {
+  if (item.cover) {
+    item.style.backgroundColor = 'grey';
+  } else item.style.background = null;
+}
+
+// Run the programme
 
 pictures.forEach(function (item) {
   // Shuffle the pictures
   item.style.order = Math.ceil(Math.random() * 12);
-  // Define the functions used later
-  function display() {
-    if (item.cover === true) {
-      item.style.backgroundColor = 'grey';
-    } else item.style.background = null;
-  }
   // 'Cover' the pictures
   item.cover = true;
-  display();
+  display(item);
   // 'Uncover'them after click and save for comparision
   item.addEventListener('click', function () {
     item.cover = false;
-    display();
+    display(item);
     compare.push(item);
     // Run the comparison
     comparison();
+    console.log(points);
   });
   return pictures;
 });
 
 function comparison() {
-  // Define the function used later
   function cover() {
     pictures.forEach(function (item) {
       item.cover = true;
-      function display() {
-        if (item.cover === true) {
-          item.style.backgroundColor = 'grey';
-        } else item.style.background = null;
-      }
-      display();
+      display(item);
     });
   }
   // Start comparison when there are 2 pictures visible
   if (compare.length === 2) {
     // Compare the pictures
-    if (compare[0].classList.value === compare[1].classList.value) {
-      // If they are same, log it and cover them again
-      console.log('Máme stejnou barvu');
-      compare.splice(0, 2);
-      setTimeout(function () {
-        cover();
-      }, 1000);
+    if (compare[0].classList.value !== compare[1].classList.value) {
       // If they are different, log it and cover them again
-    } else {
       console.log('Máme jinou barvu');
       compare.splice(0, 2);
       setTimeout(function () {
         cover();
       }, 1000);
+    } else {
+      // If they are same, log it, let them disappear and add a point
+      points++;
+      showPoints();
+      console.log('Máme stejnou barvu');
+      compare[0].classList.add('found');
+      compare[1].classList.add('found');
+
+      // START HERE TOMORROW!
+      // setTimeout(function () {
+      //   compare[0].style.backgroundColor = '0';
+      //   compare[1].style.backgroundColor = '0';
+      // }, 1000);
+      compare.splice(0, 2);
     }
   }
 }
