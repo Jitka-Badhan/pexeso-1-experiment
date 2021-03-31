d4418b1b507ef9c7b6a99196ec28c3ad0847c8a0
Jitka Badhan Nebeska <jitkabadhan@gmail.com>
2021-03-31 11:35:54 +0200

    Simplified script

--- script.js
+++ script.js
@@ -15,74 +15,54 @@ const pictures = [
   document.getElementById('pic11'),
   document.getElementById('pic12'),
 ];
-const pointsDiv = document.getElementById('points');
 let compare = [];
 let points = 0;
 
-// Propojené počítadlo:
-const showPoints = () => {
+// Main functions of the programme
+const refreshPoints = () => {
+  const pointsDiv = document.getElementById('points');
   pointsDiv.textContent = `Body: ${points}`;
 };
-showPoints();
-
-// Define the functions used later in global scope
-function display(item) {
-  if (item.cover) {
-    item.style.backgroundColor = 'grey';
-  } else item.style.background = null;
-}
-
-// Run the programme
-
-pictures.forEach(function (item) {
-  // Shuffle the pictures
-  item.style.order = Math.ceil(Math.random() * 12);
-  // 'Cover' the pictures
-  item.cover = true;
-  display(item);
-  // 'Uncover'them after click and save for comparision
-  item.addEventListener('click', function () {
-    item.cover = false;
-    display(item);
-    compare.push(item);
-    // Run the comparison
-    comparison();
-    console.log(points);
-  });
-  return pictures;
-});
 
 function comparison() {
-  function cover() {
-    pictures.forEach(function (item) {
-      item.cover = true;
-      display(item);
-    });
-  }
-  // Start comparison when there are 2 pictures visible
-  if (compare.length === 2) {
-    // Compare the pictures
-    if (compare[0].classList.value !== compare[1].classList.value) {
-      // If they are different, log it and cover them again
-      console.log('Máme jinou barvu');
-      compare.splice(0, 2);
-      setTimeout(function () {
-        cover();
-      }, 1000);
-    } else {
-      // If they are same, log it, let them disappear and add a point
-      points++;
-      showPoints();
-      console.log('Máme stejnou barvu');
+  if (compare[0].classList.value !== compare[1].classList.value) {
+    // If 2 pics are different, cover them
+    setTimeout( function() {
+      compare[0].classList.add('cover');
+      compare[1].classList.add('cover');
+      compare.splice(0, 2); 
+    }, 1000);
+  } else {
+    // If 2 pics are same, let them disappear and add a point
+    points++;
+    refreshPoints();
+    setTimeout( function() {
       compare[0].classList.add('found');
       compare[1].classList.add('found');
-
-      // START HERE TOMORROW!
-      // setTimeout(function () {
-      //   compare[0].style.backgroundColor = '0';
-      //   compare[1].style.backgroundColor = '0';
-      // }, 1000);
-      compare.splice(0, 2);
-    }
+      compare.splice(0, 2); 
+    }, 1000);
   }
 }
+
+function init () {
+  pictures.forEach(function (item) {
+    // Shuffle the pictures
+    item.style.order = Math.ceil(Math.random() * 10);
+    // 'Cover' the pictures
+    item.classList.add('cover');
+    // 'Uncover'them after click and compare when there are 2 pics uncovered
+    item.addEventListener('click', function () {
+      item.classList.remove('cover');
+      compare.push(item);
+      if (compare.length === 2) {
+        (compare[0].id !== compare[1].id) ? comparison() : compare.pop();
+      };
+    });
+  })
+  refreshPoints()
+}
+
+// Run the programme 
+init();
+
+
